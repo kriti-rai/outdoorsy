162f09af1c9e7ed74e875e20f6baf7adbf7317b4
Kriti Rai <kriti.rai@gmail.com>
2018-03-30 21:38:46 +0000

    Added greetings in CLI

--- lib/outdoorsy/CLI.rb
+++ lib/outdoorsy/CLI.rb
@@ -2,6 +2,33 @@
 class Outdoorsy::CLI
 
   def call
-    puts "Hello"
+    puts <<~HEREDOC
+                    ||                      ||
+                  ||||||                  ||||||
+        =========||||||||================||||||||=========
+                  ||||||                  ||||||
+                    ||                      ||
+    HEREDOC
+    sleep(1)
+    puts "Tired of your old workout routine? Need some guidance or something new?"
+    sleep(1.5)
+    puts "Look no further."
+    sleep(1.5)
+    puts "SwoleMate has got your back!"
+    sleep(1.5)
+    puts"We provide you with a list of latest in workouts that you can include in your routine to attain your dream body."
+    sleep (1.5)
+    puts <<~HEREDOC
+        +++++++++++++++++++++++++++++++++++++++++++++++++++
+        +                                                 +
+        +              Let’s get swole!                   +
+        +                                                 +
+        +++++++++++++++++++++++++++++++++++++++++++++++++++
+    HEREDOC
+    sleep (1.5)
+    puts "Type “list” to check out the latest in workout."
+  end
+
+  def list
   end
 end
