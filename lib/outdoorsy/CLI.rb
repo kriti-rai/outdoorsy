76cb63c57c334506ce397a06c181b7e1ef51bb44
Kriti Rai <kriti.rai@gmail.com>
2018-04-01 05:08:35 +0000

    added #article, #content, #workouts and #exit methods

--- lib/outdoorsy/CLI.rb
+++ lib/outdoorsy/CLI.rb
@@ -28,10 +28,16 @@ class Outdoorsy::CLI
     sleep (1.5)
     puts "Type “list” to check out the latest in workout."
     sleep(1.5)
-    list
+    list_articles
+    sleep(1.5)
+    article
+    sleep (1)
+    workouts
+    sleep (1)
+    exit
   end
 
-  def list
+  def list_articles
     puts <<~HEREDOC
         1. What Makes Dropsets So Good, But So Nasty?
         2. 4 Tips For Staying Fit While Traveling
@@ -41,4 +47,37 @@ class Outdoorsy::CLI
         Type "more" to see more. (Shows 10 at a time and puts "that is the end of menu if no more articles to load")
     HEREDOC
   end
+
+  def article
+    puts "Enter the number for the article you are interested in"
+    #gets input
+    #shows the article content
+  end
+
+  def content
+    #gives users the URL to view the content
+
+
+  end
+
+  def workouts
+    #lists specific workouts in the article and gives users to get instructions on each workout
+    puts "Please enter the number for the workout you are interested in or enter 'exit' to exit"
+    input = nil
+    while input != "exit"
+      input = gets.strip.downcase
+      case input
+      when "1"
+        puts "Showing the workout #{input}..."
+      when input == "2"
+        "Showing the workout #2.."
+      end
+    end
+  end
+
+  def exit
+    puts "Goodbye. Keep up the good work and stay swole! See you soon!"
+  end
+
+
 end
