f871c4c0803c074ad2cfb3c3213f2f752eddf002
Kriti Rai <kriti.rai@gmail.com>
2018-04-01 03:54:38 +0000

    Added #list method

--- lib/outdoorsy/CLI.rb
+++ lib/outdoorsy/CLI.rb
@@ -27,8 +27,18 @@ class Outdoorsy::CLI
     HEREDOC
     sleep (1.5)
     puts "Type “list” to check out the latest in workout."
+    sleep(1.5)
+    list
   end
 
   def list
+    puts <<~HEREDOC
+        1. What Makes Dropsets So Good, But So Nasty?
+        2. 4 Tips For Staying Fit While Traveling
+        3. This Is Full-Body Training Done Right!
+        10. Your Guide to Building Next-Level Legs
+
+        Type "more" to see more. (Shows 10 at a time and puts "that is the end of menu if no more articles to load")
+    HEREDOC
   end
 end
