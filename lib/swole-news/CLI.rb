0acd48759b3e6b5c443076d22877c467b6dbf7ed
Kriti Rai <kriti.rai@gmail.com>
2018-04-05 06:30:42 +0000

    Added a line to puts if the user wants to view the workouts listed inside the article or exit.

--- lib/swole-news/CLI.rb
+++ lib/swole-news/CLI.rb
@@ -58,17 +58,22 @@ class SwoleNews::CLI
     #asks for input
     #calls Article.find_by_number(input) to return the article by number
     #calls Scraper.scrape_workouts(artile_url) to return the list of workouts
-    puts "Please enter a number from the list to view the workouts listed inside or enter 'exit' to exit"
+    # puts "Please enter a number from the list to view the workouts listed inside or enter 'exit' to exit"
 
-    input = gets.strip.downcase
-    if input.to_i.between?(1, SwoleNews::Article.all.size)
-      article = SwoleNews::Article.find_by_number(input)
-      puts "#{article.title}".colorize(:red) + " * #{article.read_time} *"
-    else
-      puts "Invalid. Please make sure you are either typing the article number or 'exit'."
-      list_workouts
+    input = nil
+    while input != "exit"
+      puts "Please enter a number from the list to view the workouts listed inside or enter 'exit' to exit"
+      input = gets.strip.downcase
+      if input.to_i.between?(1, SwoleNews::Article.all.size)
+        article = SwoleNews::Article.find_by_number(input)
+        puts "#{article.title}".colorize(:red) + " * #{article.read_time} *"
+        puts "Would you like to view the workouts listed inside the article?"
+        puts "If yes, please type 'workout' or 'exit' to exit"
+      else
+        puts "Invalid. Please make sure you are either typing the article number or 'exit'."
+        list_workouts
+      end
     end
-
   end
 
   def goodbye
