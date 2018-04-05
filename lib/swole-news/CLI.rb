8ddff3ee94e27c9481acc1c7f5e30b4cff4705b7
Kriti Rai <kriti.rai@gmail.com>
2018-04-05 06:15:54 +0000

    Deleted the stub Added a line to check if the input when converted to an integer is in between the given range If yes, puts a brief info on the article Call list_workouts method again in case of an invalid entry to repeat the method

--- lib/swole-news/CLI.rb
+++ lib/swole-news/CLI.rb
@@ -56,38 +56,19 @@ class SwoleNews::CLI
 
   def list_workouts
     #asks for input
-    #gets and converts the input to integer
     #calls Article.find_by_number(input) to return the article by number
     #calls Scraper.scrape_workouts(artile_url) to return the list of workouts
-
-
     puts "Please enter a number from the list to view the workouts listed inside or enter 'exit' to exit"
 
     input = gets.strip.downcase
-    # while input != "exit"
-      if input.between?(1,SwoleNews::Article.all.size)
-        SwoleNews::Article.find_by_number(input)
-      else
-        puts "Invalid. Please make sure you are either typing the article number or 'exit'."
-        list_workouts
-      end
+    if input.to_i.between?(1, SwoleNews::Article.all.size)
+      article = SwoleNews::Article.find_by_number(input)
+      puts "#{article.title}".colorize(:red) + " * #{article.read_time} *"
+    else
+      puts "Invalid. Please make sure you are either typing the article number or 'exit'."
+      list_workouts
     end
-    #lists specific workouts in the article and gives users to get instructions on each workout
-    # input = nil
-    # while input != "exit"
-    #   puts "Please enter the number for the workout you are interested in or enter 'menu' to go back to the main menu. Enter 'exit' to exit"
-    #   input = gets.strip.downcase
-    #   case input
-    #   when "1"
-    #     puts "Showing the workout #{input}..."
-    #   when "2"
-    #     puts "Showing the workout #{input}..."
-    #   when "menu"
-    #     main_menu
-    #   else
-    #     puts "Invalid. Please make sure you are either typing the workout number, menu or exit."
-    #   end
-    # end
+
   end
 
   def goodbye
