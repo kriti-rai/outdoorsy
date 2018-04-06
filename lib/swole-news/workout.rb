88ce46599383da24543652a0b20583c3b1281bae
Kriti Rai <kriti.rai@gmail.com>
2018-04-06 03:45:42 +0000

    Added @@all and set it to empty array Added self.all to read @@all

--- lib/swole-news/workout.rb
+++ lib/swole-news/workout.rb
@@ -1,7 +1,7 @@
 require 'pry'
 class SwoleNews::Workout
   attr_accessor :title, :definition
-
+  @@all = []
   def initialize(workout_hash)
     workout_hash.each {|k,v| self.send("#{k}=",v)}
     @@all << self
@@ -9,6 +9,10 @@ class SwoleNews::Workout
 
   def self.create_from_collection(workout_array)
     workout_array.each {|workout_hash| self.new(workout_hash)}
+  end
+
+  def self.all
+    @@all
   end
 
 
