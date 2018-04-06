955c4d7f0f028960926e63cccbd6bd15d4a47a20
Kriti Rai <kriti.rai@gmail.com>
2018-04-06 03:33:12 +0000

    Created initialize method to take an argument of workout_hash and assign attributes to the new workout object Created create_from_collection method to take in an argument of workout_array to iterate over the array and create new workout objects calling the initialize method

--- lib/swole-news/workout.rb
+++ lib/swole-news/workout.rb
@@ -2,9 +2,14 @@ require 'pry'
 class SwoleNews::Workout
   attr_accessor :title, :definition
 
+  def initialize(workout_hash)
+    workout_hash.each {|k,v| self.send("#{k}=",v)}
+    @@all << self
+  end
 
-
-
+  def self.create_from_collection(workout_array)
+    workout_array.each {|workout_hash| self.new(workout_hash)}
+  end
 
 
 end
