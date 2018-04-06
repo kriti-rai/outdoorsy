190af7016eb67f6a5648e5c56d8384d891f9f2dd
Kriti Rai <kriti.rai@gmail.com>
2018-04-06 03:20:20 +0000

    Set an empty workout_hash in scrape_workouts(article_url) method Set the method to iterate over the workout content wrapper and written workout_hash at the end

--- lib/swole-news/scraper.rb
+++ lib/swole-news/scraper.rb
@@ -23,15 +23,16 @@ class SwoleNews::Scraper
 
   def self.scrape_workouts(article_url)
   #scrapes the articles to list out an array of workouts which might have hashes of supersets
-
-    # self.scrape_page.map do |article|
-    #   article[:workouts] = []
-    #   article[:workouts] << Nokogiri::HTML(open(article[:url])).search(".cms-article-workout__exercise--title")
-    #   # binding.pry
-    # end
+    workout_hash = {}
     doc = Nokogiri::HTML(open(article_url))
-    title = doc.search(".cms-article-workout__exercise--title").text
-    definition = doc.search(".cms-article-workout__sets--definition").text
+    doc.search(".cms-article-list__content--wrapper").each do |workout|
+      workout = {
+        :title => doc.search(".cms-article-workout__exercise--title").text,
+        :definition => doc.search(".cms-article-workout__sets--definition").text.strip
+        }
+      end
+      workout_hash
+    end
   end
 
 end
