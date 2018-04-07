0107cd49601ddd2e3f3d7cdc764e86eb6a1d8eec
Kriti Rai <kriti.rai@gmail.com>
2018-04-07 04:10:16 +0000

    Cleaned up

--- lib/swole-news/scraper.rb
+++ lib/swole-news/scraper.rb
@@ -4,20 +4,16 @@ class SwoleNews::Scraper
     #scrapes the URL and gives an array of articles in scraped_articles
     article_array = []
     doc = Nokogiri::HTML(open('https://www.bodybuilding.com/category/workouts'))
-    #title = doc.search(".cms-article-list--article.col.col-1 h3.title").text
-    #url = doc.css(".cms-article-list--article.col.col-1 a").attribute('href').to_s
-    #read_time = doc.search(".bb-read-time__time").text
-    #description = doc.search("span.description").text
     doc.css(".cms-article-list--article.col.col-1").map do |article|
       article = {
         :title => article.search("h3.title").text,
         :url => article.search("a").attribute('href').to_s,
         :read_time => article.search(".bb-read-time__time").text,
         :description => article.search("span.description").text
-      }
+        }
         article_array << article
       end
-      article_array
+    article_array
   end
 
   def self.scrape_workouts(article_url)
