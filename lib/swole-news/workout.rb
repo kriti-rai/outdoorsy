2dea2092d77ce2bded20372452eb90947beceb80
Kriti Rai <kriti.rai@gmail.com>
2018-04-07 06:08:24 +0000

    Added @@all and self.all back

--- lib/swole-news/workout.rb
+++ lib/swole-news/workout.rb
@@ -1,13 +1,18 @@
 require 'pry'
 class SwoleNews::Workout
   attr_accessor :title, :definition, :article
-
+  @@all =[]
   def initialize(workout_hash)
     workout_hash.each {|k,v| self.send("#{k}=",v)}
+    @@all << self
   end
 
   def self.create_from_collection(workout_array)
     workout_array.each {|workout_hash| self.new(workout_hash)}
+  end
+
+  def self.all
+    @@all
   end
 
 end
