79175cecb85d9b6814377459f28dfb27c36d5899
Kriti Rai <kriti.rai@gmail.com>
2018-04-10 05:32:43 +0000

    Changed each to map in create_from_collection

--- lib/swole-news/workout.rb
+++ lib/swole-news/workout.rb
@@ -5,11 +5,11 @@ class Workout
 
   def initialize(workout_hash)
     workout_hash.each {|k,v| self.send("#{k}=",v)}
-    article.add_workout(self)
+    # article.add_workout(self)
   end
 
   def self.create_from_collection(workout_array)
-    workout_array.each {|workout_hash| self.new(workout_hash)}
+    workout_array.map {|workout_hash| self.new(workout_hash)}
   end
 
   def self.all
